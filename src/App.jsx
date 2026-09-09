d113270f39330a88f362d957a14180109e9606c0
ItGencer <gencer.it.1989@gmail.com>
2026-09-09 23:22:31 +0300

    add section about us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.scss";
 import Header from "./components/layout/header/header";
 import Hero from "./components/hero/hero";
+import AboutUs from "./components/about-us/about-us";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <AboutUs />
       </main>
     </>
   );
